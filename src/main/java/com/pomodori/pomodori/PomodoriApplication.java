865d42f06917720b9d3268f850f8eea98769923e
Emmanuell <emmanuellnogmiq@hotmail.com>
2019-08-02 10:44:32 -0500

    initial commit setup files.

---
+++ src/main/java/com/pomodori/pomodori/PomodoriApplication.java
@@ -0,0 +1,13 @@
+package com.pomodori.pomodori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomodoriApplication.class, args);
+	}
+
+}
